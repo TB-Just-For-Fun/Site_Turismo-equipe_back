126ef7b051ee8a05d85c128fb4cb4ba132934cea
Niosemiro <niosemirol@gmail.com>
2024-09-20 14:09:52 +0100

    commit push

---
+++ chat/src/server.ts
@@ -0,0 +1,44 @@
+import express, { Application } from "express";
+import http from 'http';
+import { Server } from 'socket.io';
+class App {
+
+    private app: Application;
+    private http: http.Server;
+    private io: Server;
+
+    constructor() {
+        this.app = express(); //instanciar uma classe
+        this.http = http.createServer(this.app);
+        this.io = new Server(this.http);
+        this.listenSocket();
+        this.setupRouters();
+    }
+
+    listenServer() {
+        this.http.listen(3000, (err?: Error) => {
+            if (err) {
+                console.error('Erro ao iniciar o servidor:', err.message);
+            } else {
+                console.log('O servidor está rodando na porta 3000');
+            }
+        });
+    }
+    listenSocket() {
+        this.io.on('connection', (socket)=>{
+            console.log('user connected =>', socket.id);
+
+            socket.on('message', (msg) => {
+this.io.emit('message', msg)
+            });
+        });
+    }
+    setupRouters() {
+        this.app.on('/', (req, res) => {
+            res.sendFile(__dirname + './');
+        });
+    }
+}
+const app = new App();
+
+app.listenServer();
