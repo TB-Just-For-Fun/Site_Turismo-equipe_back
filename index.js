d957904a2a7ccbba7c7b07950c662e4ba5f1888e
JoseSilva009 <josepaulinoalessacdasilva@gmail.com>
2024-10-09 16:13:51 +0100

    Update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cors());
 //adicionando o header ao servidor
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     next()
 })
 
